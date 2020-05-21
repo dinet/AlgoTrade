99ffc91d826836242d6f09c03eaeb00d342bff65
Dineth <dineth@live.com>
2020-05-20 19:06:30 -0500

    Added bar charts

--- plotter.py
+++ plotter.py
@@ -1,29 +1,108 @@
-import datetime
-import pandas_datareader.data as web
-import matplotlib.pyplot as plot
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+from pandas_datareader import data as web
 import plotly.graph_objects as go
 
-start = datetime.datetime(2018, 5, 1)
-end = datetime.datetime.now()
-df = web.DataReader("hcci", 'yahoo', start, end)
-df["100ma"] = df['Adj Close'].rolling(window=100,min_periods=0).mean()
-df["50ma"] = df['Adj Close'].rolling(window=10,min_periods=0).mean()
-# df["Adj Close"].plot()
-# df["Close"].plot()
-ax1 = plot.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
-ax2 = plot.subplot2grid((6,1),(5,0),rowspan=1,colspan=1,sharex=ax1)
 
-ax1.plot(df.index,df['Adj Close'])
-ax1.plot(df.index,df['100ma'])
-ax1.plot(df.index,df['50ma'])
-ax2.bar(df.index,df['Volume'])
-plot.show()
-# print(df.tail())
-# print(df.head())
-# df.head()
-# plot.show()
-# df.reset_index(inplace=True)
-# df.set_index("Date", inplace=True)
-# df = df.drop("Symbol", axis=1)
+# In[29]:
 
-# print(df.head())
+
+stock = 'hcci'
+ 
+df = web.DataReader(stock, data_source='yahoo', start='01-01-2019')
+
+df.to_csv('test.csv')
+
+
+# In[35]:
+
+
+trace1 = {
+    'x': df.index,
+    'open': df.Open,
+    'close': df.Close,
+    'high': df.High,
+    'low': df.Low,
+    'type': 'candlestick',
+    'name': 'MSFT',
+    'showlegend': False
+}
+
+
+# In[24]:
+
+
+# Calculate and define moving average of 30 periods
+avg_30 = df.Close.rolling(window=30, min_periods=1).mean()
+
+# Calculate and define moving average of 50 periods
+avg_50 = df.Close.rolling(window=50, min_periods=1).mean()
+
+
+# In[25]:
+
+
+trace2 = {
+    'x': df.index,
+    'y': avg_30,
+    'type': 'scatter',
+    'mode': 'lines',
+    'line': {
+        'width': 1,
+        'color': 'blue'
+            },
+    'name': 'Moving Average of 30 periods'
+}
+
+
+# In[34]:
+
+
+
+
+
+# In[26]:
+
+
+trace3 = {
+    'x': df.index,
+    'y': avg_50,
+    'type': 'scatter',
+    'mode': 'lines',
+    'line': {
+        'width': 1,
+        'color': 'red'
+    },
+    'name': 'Moving Average of 50 periods'
+}
+
+
+# In[37]:
+
+
+data = [trace1, trace2, trace3]
+# Config graph layout
+layout = go.Layout({
+    'title': {
+        'text': 'Microsoft(MSFT) Moving Averages',
+        'font': {
+            'size': 15
+        }
+    }
+})
+
+
+# In[38]:
+
+
+fig = go.Figure(data=data, layout=layout)
+fig.write_html("Microsoft(MSFT) Moving Averages.html")
+fig.show()
+
+
+# In[ ]:
